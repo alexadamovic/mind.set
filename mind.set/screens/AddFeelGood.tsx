dc0102a70aad1b69fd8bb0d35a5257bfcc7979d3
Alex Adamovic <alexanderadamovic@gmail.com>
2022-05-12 14:53:03 -0700

    Add type field with checkboxes to Feel Good

--- mind.set/screens/AddFeelGood.tsx
+++ mind.set/screens/AddFeelGood.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button } from 'react-native-elements';
+import { useAuthentication } from '../utils/hooks/useAuthentication';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { db } from '../config/firebase';
 import { collection, addDoc } from 'firebase/firestore';
+import { CheckBox } from '@rneui/themed';
 
 const AddFeelGoodScreen = () => {
+  const { user } = useAuthentication();
 
   const [value, setValue] = React.useState({
     type: '',
@@ -16,7 +19,8 @@ const AddFeelGoodScreen = () => {
   async function Create() {
     await addDoc(collection(db, "Feel Goods"), {
       type: 'WHATEVER',
-      content: value.content
+      content: value.content,
+      uid: user?.uid
     });
   }
 
@@ -35,8 +39,62 @@ const AddFeelGoodScreen = () => {
   //   })
 
 
+  const [check1, setCheck1] = React.useState(false);
+  const [check2, setCheck2] = React.useState(false);
+  const [check3, setCheck3] = React.useState(false);
+  const [check4, setCheck4] = React.useState(false);
+  const [check5, setCheck5] = React.useState(false);
+
+
   return (
     <View style={styles.container}>
+      <Text>What kind of feel good is this??? (Select One)</Text>
+
+      <CheckBox
+        center
+        title="Memory"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={check1}
+        onPress={() => setCheck1(!check1)}
+      />
+
+      <CheckBox
+        center
+        title="Compliment"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={check2}
+        onPress={() => setCheck2(!check2)}
+      />
+
+      <CheckBox
+        center
+        title="Achievement"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={check3}
+        onPress={() => setCheck3(!check3)}
+      />
+
+      <CheckBox
+        center
+        title="Encouragement"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={check4}
+        onPress={() => setCheck4(!check4)}
+      />
+
+      <CheckBox
+        center
+        title="Other"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={check5}
+        onPress={() => setCheck5(!check5)}
+      />
+
       <Input
         placeholder='What makes you feel good??'
         multiline
