d107268e793d25ef5126a5034f26e9d2e9f1a3b4
Alex Adamovic <alexanderadamovic@gmail.com>
2022-05-13 10:12:25 -0700

    WIP: Bugfix document retrieval from Firestore

--- mind.set/screens/MoodBooster.tsx
+++ mind.set/screens/MoodBooster.tsx
@@ -4,10 +4,22 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button } from 'react-native-elements';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { db } from '../config/firebase';
-import { collection, query, where } from "firebase/firestore"
+import { collection, query, where, getDocs } from "firebase/firestore"
 
 const feelGoods = collection(db, "Feel Goods")
 const q = query(feelGoods, where("type", "==", "Memory"));
+
+console.log(feelGoods);
+
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+
+// querySnapshot.forEach((doc) => {
+//   console.log(doc.id, " => ", doc.data();)
+// });
 
 const MoodBoosterScreen = () => {
   return (
