8100f91088df705609777a851f5a697a393139f1
Alex Adamovic <alexanderadamovic@gmail.com>
2022-05-13 11:51:27 -0700

    WIP: retrieve random document from ourArray

--- mind.set/screens/MoodBooster.tsx
+++ mind.set/screens/MoodBooster.tsx
@@ -4,23 +4,47 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button } from 'react-native-elements';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { db } from '../config/firebase';
-import { collection, query, where, getDocs, DocumentData } from "firebase/firestore"
+import { collection, query, where, getDocs, DocumentData } from "firebase/firestore";
+import { useAuthentication } from '../utils/hooks/useAuthentication';
 
-const feelGoods = collection(db, "Feel Goods")
-const q = query(feelGoods, where("type", "==", "Memory"));
-let ourArray: DocumentData[] = [];
+// const { user } = useAuthentication();
+// const feelGoods = collection(db, "Feel Goods")
+// const q = query(feelGoods, where("uid", "==", user?.uid), where("type", "==", "Memory"));
+// let ourArray: DocumentData[] = [];
 
-async function WTH() {
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    ourArray.push(doc.data());
-  });
-};
+// async function WTH() {
+//   const querySnapshot = await getDocs(q);
+//   querySnapshot.forEach((doc) => {
+//     ourArray.push(doc.data());
+//   });
+// };
 
-WTH();
-console.log(ourArray);
+// WTH();
+// console.log(ourArray);
 
 const MoodBoosterScreen = () => {
+
+  const { user } = useAuthentication();
+  const feelGoods = collection(db, "Feel Goods")
+  const q = query(feelGoods, where("type", "==", "Memory"));
+  let ourArray: DocumentData[] = [];
+  let ourObject: String = ourArray[0].toString();
+
+  async function WTH() {
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      ourArray.push(doc.data());
+    });
+  };
+  WTH();
+
+  console.log(ourObject);
+
+  function getRandomFeelGood() {
+    // return ourArray[Math.floor(Math.random() * (ourArray.length - 1))]
+    return ourArray[0].content;
+  }
+
   return (
     <View style={styles.container}>
       <Text>You're doing AWESOME!</Text>
