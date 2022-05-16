bd6339200f077a197f57645462321ed6669e83ee
Alex Adamovic <alexanderadamovic@gmail.com>
2022-05-16 14:31:41 -0700

    WIP: Add custom styling

--- mind.set/screens/MoodBooster.tsx
+++ mind.set/screens/MoodBooster.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'react-native-elements';
+import { Input, Card, Button } from 'react-native-elements';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { db } from '../config/firebase';
 import { collection, query, where, getDocs, DocumentData } from "firebase/firestore";
 import { useAuthentication } from '../utils/hooks/useAuthentication';
 import Constants from 'expo-constants';
+import from '../styles/main';
 
 
 const MoodBoosterScreen = () => {
@@ -14,16 +15,20 @@ const MoodBoosterScreen = () => {
   const { user } = useAuthentication();
   const feelGoods = collection(db, "Feel Goods");
   const [text, setText] = React.useState("Let's take a trip down memory lane")
+  const [title, setTitle] = React.useState("Type of Feel Good")
 
   async function fetch() {
     const q = query(feelGoods, where("uid", "==", user?.uid));
     let ourArray: DocumentData[] = [];
+    let randomDoc: DocumentData = {};
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       ourArray.push(doc.data());
     });
-    setText(ourArray[Math.floor(Math.random() * ourArray.length)].content);
+    randomDoc = ourArray[Math.floor(Math.random() * ourArray.length)];
+    setText(randomDoc.content);
+    setTitle(randomDoc.type);
   }
 
   async function callOpenAi() {
@@ -46,9 +51,15 @@ const MoodBoosterScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Fetch" onPress={fetch}></Button>
-      <Button title="OPENAI" onPress={callOpenAi}></Button>
+
+      <Card>
+        <Card.Title>{title}</Card.Title>
+        <Card.Divider />
+        <Text>{text}</Text>
+      </Card>
+
+      <Button buttonStyle={styles.button} title="Fetch Random" onPress={fetch}></Button>
+      <Button buttonStyle={styles.button} title="OPENAI" onPress={callOpenAi}></Button>
     </View>
   );
 };
@@ -62,7 +73,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10
-  }
+  },
 });
 
 export default MoodBoosterScreen;
